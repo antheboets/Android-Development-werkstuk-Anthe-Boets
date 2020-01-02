745035c74e20cb6dad2914f8db47c9d5eb9b2950
antheboets <44194665+antheboets@users.noreply.github.com>
2020-01-02 04:12:38 +0100

    adding edit days fragment

---
+++ app/src/main/java/com/example/werkstuk/Fragments/SetDaysFragment.java
@@ -0,0 +1,52 @@
+package com.example.werkstuk.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.werkstuk.AddTimeInstanceActivity;
+import com.example.werkstuk.R;
+
+public class SetDaysFragment extends Fragment {
+
+    public interface SetDaysFragmentListener {
+
+    }
+    private SetDaysFragmentListener setDaysFragmentListener;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.time_instance_edit_days_fragment, container, false);
+
+
+        return view;
+    }
+
+    /*
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof TimeInstanceListFragment.TimeInstanceFragmentListListener){
+            setDaysFragmentListener = (SetDaysFragment.SetDaysFragmentListener) context;
+        }
+        else {
+            throw new RuntimeException(context.toString() + " must implement TimeInstanceFragmentListListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        setDaysFragmentListener = null;
+    }
+    */
+
+}
