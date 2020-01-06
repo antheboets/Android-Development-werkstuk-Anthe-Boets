c742326e9de82a590e3810b0a3cbe793760746fd
antheboets <44194665+antheboets@users.noreply.github.com>
2020-01-06 13:45:30 +0100

    Update BootCompletedBroadcastReceiver.java

--- app/src/main/java/com/example/werkstuk/BootCompletedBroadcastReceiver.java
+++ app/src/main/java/com/example/werkstuk/BootCompletedBroadcastReceiver.java
@@ -12,12 +12,12 @@ import com.example.werkstuk.Database.TimeInstanceRepository;
 
 import java.util.List;
 
+import static com.example.werkstuk.AddEditTimeInstanceActivity.EXTRA_ID;
+
 public class BootCompletedBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-
         if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
 
             TimeInstanceRepository repository = new TimeInstanceRepository(((Application) context.getApplicationContext()));
@@ -27,6 +27,7 @@ public class BootCompletedBroadcastReceiver extends BroadcastReceiver {
                 for (TimeInstance item: timeInstanceList) {
                     if(item.isOn()&& item.hasADay()){
                         Intent alarmIntent = new Intent(context, AlarmReceiver.class);
+                        alarmIntent.putExtra(EXTRA_ID, item.getId());
                         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, item.getId(), alarmIntent,0);
                         alarmManager.set(AlarmManager.RTC_WAKEUP, item.calMiniSecForNextAlarm(),pendingIntent);
                     }
